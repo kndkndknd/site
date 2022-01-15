5e0378bbd3610449e01afffafde57fcecc61a79f
knd <knd@knd.cloud>
2022-01-16 00:09:59 +0900

    20220116

--- components/layout.tsx
+++ components/layout.tsx
@@ -22,11 +22,7 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="knd" />
-        <meta name="twitter:title" content="knd.cloud" />
-        <meta name="twitter:description" content="knd" />
-        <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1365947109271162884/tIlGQtVd_400x400.jpg" />
+        <meta property="og:image" content="https://knd.cloud/images/profile.jpg" />
       </Head>
       <header className={styles.header}>
         {home ? (
