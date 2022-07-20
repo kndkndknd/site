c1f236f4b3a22905dd8f2656e7a305dfff273bd9
knd <knd@knd@cloud>
2022-07-20 22:03:50 +0900

    20220720 zenn-embed-elementsのエラーをでなくした

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import { AppProps } from 'next/app'
 import '../styles/global.css'
 import { useEffect } from 'react';
-import initTwitterScriptInner from 'zenn-embed-elements/lib/init-twitter-script-inner';
-
+//import initTwitterScriptInner from 'zenn-embed-elements/lib/init-twitter-script-inner';
+// zenn-embed-elementsからinitTwitterScriptInnerがなくなってたのでいったん直接定義
+const initTwitterScriptInner = `window.twttr=(function(f,b,g){var e,c=f.getElementsByTagName(b)[0],a=window.twttr||{};if(f.getElementById(g)){return a}e=f.createElement(b);e.id=g;e.src="https://platform.twitter.com/widgets.js";c.parentNode.insertBefore(e,c);a._e=[];a.ready=function(d){a._e.push(d)};return a}(document,"script","twitter-wjs"));`;
 export default function App({ Component, pageProps, router }: AppProps) {
   useEffect(()=> {import("zenn-embed-elements")},[])
 
